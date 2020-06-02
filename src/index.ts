18aae4f00d6c7c6e44efdcbe19945a17f4fb27fa
William L <w-lindvall@outlook.com>
2020-06-02 09:47:44 -0700

    copy to clipboard

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,39 @@
 import {Command} from '@oclif/command'
+import {write as copyToClipboard} from 'clipboardy'
+import {joinWithNewline} from './utils'
 import {lorem} from './lorem'
 
 class LoremCli extends Command {
-  static description = 'generate placeholder text'
+  static description = "generate and copy 'lorem ipsum' placeholder text"
 
   static args = [
     {
       name: 'paragraphs',
       required: false,
-      description: 'number of paragraphs (150 max)',
+      description: 'number of paragraphs (1-150)',
       default: 1,
     },
   ]
 
   async run() {
     const {args} = this.parse(LoremCli)
+    const {paragraphs} = args
 
-    if (args.paragraphs > 150) {
-      console.log('that seems excessive')
-    } else {
-      const targetParagraphs = lorem.slice(0, args.paragraphs)
-      const textStr = targetParagraphs.join(`
-`)
-      console.log(textStr)
+    if (paragraphs > 150) this.error(new Error('that seems excessive'))
+
+    const targetParagraphs = lorem.slice(0, paragraphs)
+    const textStr = joinWithNewline(targetParagraphs)
+
+    this.log(textStr)
+
+    try {
+      await copyToClipboard(textStr)
+      this.log('\x1b[32m', '(copied to clipboard)')
+    } catch (error) {
+      this.warn('(unable to access clipboard)')
     }
+
+    this.exit()
   }
 }
 
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export const joinWithNewline = (textArray: string[]): string =>
+  textArray.join(`
+
+`)
